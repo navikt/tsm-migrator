f4853f2047cae0d3a601160d79db116a83ecb451
Andreas Sagen Aspaas <danduras@gmail.com>
2024-03-06 13:36:19 +0100

    chore: remove sample code

--- src/main/kotlin/no/nav/tsm/Application.kt
+++ src/main/kotlin/no/nav/tsm/Application.kt
@@ -6,13 +6,12 @@ import io.ktor.server.netty.*
 import no.nav.tsm.plugins.*
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+    embeddedServer(Netty, port = 8080, module = Application::module)
         .start(wait = true)
 }
 
 fun Application.module() {
     configureSerialization()
-    configureDatabases()
     configureRouting()
     configureMonitoring()
 }
