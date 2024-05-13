f27f2630a6c5ef714df7d24be5315f6ba4eb5537
Jørn-Are Flaten <ja.flaten91@gmail.com>
2024-05-13 14:13:18 +0200

    chore: adding secure log to GamleSykmeldingerDeserializer

--- src/main/kotlin/no/nav/tsm/sykmeldinger/kafka/util/GamleSykmeldingerDeserializer.kt
+++ src/main/kotlin/no/nav/tsm/sykmeldinger/kafka/util/GamleSykmeldingerDeserializer.kt
@@ -4,8 +4,11 @@ import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import no.nav.tsm.sykmeldinger.database.GamleSykmeldingerService
 import no.nav.tsm.sykmeldinger.kafka.model.GamleSykmeldingerInput
 import org.apache.kafka.common.serialization.Deserializer
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import java.time.LocalDateTime
 import kotlin.reflect.KClass
 
@@ -17,8 +20,13 @@ class GamleSykmeldingerDeserializer<T : Any>(private val type: KClass<T>) : Dese
         configure(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT, true)
     }
 
+    companion object {
+        val securelog: Logger = LoggerFactory.getLogger("securelog")
+    }
+
     override fun deserialize(topic: String, p1: ByteArray): T {
         val jsonNode: JsonNode = objectMapper.readTree(p1)
+        securelog.info("GamleSykmeldingerDeserializer.deserialize: jsonNode = $jsonNode")
         val sykmeldingId = jsonNode.get("receivedSykmelding").get("sykmelding").get("id").asText()
         val mottattDato = LocalDateTime.parse(jsonNode.get("receivedSykmelding").get("mottattDato").asText())
         val gammelSykmelding = jsonNode.get("receivedSykmelding").get("sykmelding").asText()
