4bc422004d4b26de8d89ed67cd204ad571cd2deb
Andreas Sagen Aspaas <danduras@gmail.com>
2024-11-20 10:29:21 +0100

    chore: stop sykmelding consumer

--- src/main/kotlin/no/nav/tsm/plugins/Consumer.kt
+++ src/main/kotlin/no/nav/tsm/plugins/Consumer.kt
@@ -9,12 +9,12 @@ import no.nav.tsm.sykmeldinger.kafka.MigrertSykmeldingConsumer
 import no.nav.tsm.sykmeldinger.kafka.SykmeldingConsumer
 
 fun Application.configureConsumer(sykmeldingConsumer: SykmeldingConsumer, migrertSykmeldingConsumer: MigrertSykmeldingConsumer, sykmeldingReformatService: SykmeldingReformatService) {
-    val sykmeldingConsumerJob = launch(Dispatchers.IO) { sykmeldingConsumer.start() }
+    //val sykmeldingConsumerJob = launch(Dispatchers.IO) { sykmeldingConsumer.start() }
     val migrertSykmeldingConsumerJob = launch(Dispatchers.IO) { migrertSykmeldingConsumer.start() }
     val sykmeldingReformatJob = launch(Dispatchers.IO) { sykmeldingReformatService.start() }
 
     environment.monitor.subscribe(ApplicationStopping) {
-        sykmeldingConsumerJob.cancel()
+        //sykmeldingConsumerJob.cancel()
         migrertSykmeldingConsumerJob.cancel()
         sykmeldingReformatJob.cancel()
     }
