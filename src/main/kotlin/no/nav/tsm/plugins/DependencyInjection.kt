9f36db0295600a6d489712b037ab6e1567218739
Andreas Sagen Aspaas <danduras@gmail.com>
2025-06-18 16:11:44 +0200

    chore: remove update service

--- src/main/kotlin/no/nav/tsm/plugins/DependencyInjection.kt
+++ src/main/kotlin/no/nav/tsm/plugins/DependencyInjection.kt
@@ -3,7 +3,6 @@ package no.nav.tsm.plugins
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import no.nav.tsm.reformat.sykmelding.SykmeldingReformatService
-import no.nav.tsm.reformat.sykmelding.SykmeldingUpdateService
 import no.nav.tsm.reformat.sykmelding.model.SykmeldingRecord
 import no.nav.tsm.reformat.sykmelding.service.SykmeldingMapper
 import no.nav.tsm.smregister.models.ReceivedSykmelding
@@ -29,49 +28,12 @@ fun Application.configureDependencyInjection() {
             environmentModule(),
             sykmeldingConsumer,
             sykmeldingReformatService,
-            sykmeldingUpdateService,
         )
     }
 }
 
 fun Application.environmentModule() = module {
     single<Environment> { createEnvironment() }
-}
-
-val sykmeldingUpdateService = module {
-    single {
-        val env = get<Environment>()
-        val consumer = KafkaConsumer(Properties().apply {
-            putAll(env.kafkaConfig)
-            this[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = JacksonKafkaDeserializer::class.java.name
-            this[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java.name
-            this[ConsumerConfig.GROUP_ID_CONFIG] = "sykmelding-reformat-update-consumer"
-            this[ConsumerConfig.CLIENT_ID_CONFIG] = "${env.hostname}-sykmelding-reformat-update-consumer"
-            this[ConsumerConfig.AUTO_OFFSET_RESET_CONFIG] = "earliest"
-            this[ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG] = "true"
-            this[ConsumerConfig.MAX_POLL_RECORDS_CONFIG] = "100"
-        }, StringDeserializer(), JacksonKafkaDeserializer(ReceivedSykmelding::class))
-
-        val producer = KafkaProducer<String, SykmeldingRecord>(Properties().apply {
-            putAll(env.kafkaConfig)
-            this[ProducerConfig.ACKS_CONFIG] = "all"
-            this[ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG] = "true"
-            this[ProducerConfig.CLIENT_ID_CONFIG] = "${env.hostname}-sykmelding-reformat-update-producer"
-            this[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java.name
-            this[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = JacksonKafkaSerializer::class.java
-            this[ProducerConfig.COMPRESSION_TYPE_CONFIG] = "gzip"
-        })
-
-        val updateService = SykmeldingUpdateService(
-            kafkaConsumer = consumer,
-            sykmeldingMapper = SykmeldingMapper(),
-            kafkaProducer = producer,
-            outputTopic = env.sykmeldingOutputTopic,
-            inputTopic = env.teamsykmeldingSykmeldingTopic,
-            cluster = env.cluster
-        )
-        updateService
-    }
 }
 
 val sykmeldingReformatService = module {
