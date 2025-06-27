161e82ccd37d251b5d06cad2475a82b6c37a1776
Andreas Sagen Aspaas <danduras@gmail.com>
2025-06-27 11:58:47 +0200

    feat: sync digital sykmelding to old arc/topics

--- src/main/kotlin/no/nav/tsm/reformat/sykmelding/util/XmlStuff.kt
+++ src/main/kotlin/no/nav/tsm/reformat/sykmelding/util/XmlStuff.kt
@@ -7,12 +7,15 @@ import no.nav.helse.msgHead.XMLMsgHead
 import no.nav.helse.sm2013.HelseOpplysningerArbeidsuforhet
 import org.xml.sax.InputSource
 import java.io.StringReader
+import java.io.StringWriter
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.ZoneOffset
 import java.util.GregorianCalendar
 import javax.xml.bind.DatatypeConverter
 import javax.xml.bind.JAXBContext
+import javax.xml.bind.Marshaller
+import javax.xml.bind.Marshaller.JAXB_ENCODING
 import javax.xml.bind.Unmarshaller
 import javax.xml.parsers.SAXParserFactory
 import javax.xml.transform.Source
@@ -33,6 +36,22 @@ class XmlStuff {
             setAdapter(LocalDateTimeXmlAdapter::class.java, XMLDateTimeAdapter())
             setAdapter(LocalDateXmlAdapter::class.java, XMLDateAdapter())
         }
+
+
+    val fellesformatMarshaller: Marshaller =
+        JAXBContext.newInstance(
+            XMLEIFellesformat::class.java,
+            XMLMsgHead::class.java,
+            HelseOpplysningerArbeidsuforhet::class.java,
+        ).createMarshaller()
+            .apply { setProperty(JAXB_ENCODING, "UTF-8") }
+
+    fun marshal(input: Any): String =
+        StringWriter().use {
+            fellesformatMarshaller.marshal(input, it)
+            it.toString()
+        }
+
 
     fun unmarshal(inputMessageText: String): XMLEIFellesformat {
         // Disable XXE
