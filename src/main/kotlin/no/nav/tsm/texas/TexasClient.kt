d35c74036d80977c684b9bb22a16c07919996c4f
Andreas Sagen Aspaas <danduras@gmail.com>
2025-07-04 11:00:41 +0200

    chore: set contenttype header

--- src/main/kotlin/no/nav/tsm/texas/TexasClient.kt
+++ src/main/kotlin/no/nav/tsm/texas/TexasClient.kt
@@ -4,6 +4,9 @@ import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
+import io.ktor.server.application.Application
 
 data class TexasResponse(
     val access_token: String,
@@ -28,6 +31,7 @@ class TexasClient(
         )
 
         return httpClient.post(tokenEndpoint) {
+            contentType(ContentType.Application.Json)
             setBody(requestBody)
         }.body<TexasResponse>().access_token
 
